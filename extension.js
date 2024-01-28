76ac2830139559488ba2f1a4ebe9e09ff0ad65c5
vinayBomma <vinaysb2000@gmail.com>
2024-01-28 10:48:54 +0530

    Added command logic for access token

--- extension.js
+++ extension.js
@@ -1,6 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
+const vscode = require("vscode");
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -9,28 +9,59 @@ const vscode = require('vscode');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+  const subDisposable = [];
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log(
+    'Congratulations, your extension "hashnode-on-vscode" is now active!'
+  );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "hashnode-on-vscode" is now active!');
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with  registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand(
+    "hashnode-on-vscode.getHashnode",
+    function () {
+      // The code you place here will be executed every time your command is executed
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('hashnode-on-vscode.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+      // Display a message box to the user
+      vscode.window.showInformationMessage("Hello Hashnode Users!!");
+    }
+  );
+  subDisposable.push(disposable);
+  let addToken = vscode.commands.registerCommand(
+    "hashnode-on-vscode.addToken",
+    async () => {
+      const accessToken = await vscode.window.showInputBox({
+        prompt: "Please Enter Hashnode Access Token",
+        title: "Enter Hashnode Access Token",
+        validateInput: (token) => {
+          if (!token) {
+            return "Please enter an access token.";
+          }
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Hashnode on VSCode!');
-	});
+          if (token.length !== 36) {
+            return "Access Token must be 36 characters long.";
+          }
 
-	context.subscriptions.push(disposable);
+          return null;
+        },
+      });
+
+      if (accessToken) {
+        vscode.window.showInformationMessage("Access Token is valid!");
+      }
+    }
+  );
+  subDisposable.push(addToken);
+
+  context.subscriptions.push(...subDisposable);
 }
 
 // This method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
-}
+  activate,
+  deactivate,
+};
